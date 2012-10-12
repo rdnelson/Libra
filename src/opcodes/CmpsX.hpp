e1891226d2a25761239cc149a033bc9636ddfa28
Robert Nelson <robert@rnelson.ca>
2012-10-12 10:03:15 -0400

    Added CMPSB and CMPSW mnemonics

---
+++ src/opcodes/CmpsX.hpp
@@ -0,0 +1,29 @@
+/*-------------------------------------*\
+|
+|  File Name: CmpsX.hpp
+|
+|  Creation Date: 11-10-2012
+|
+|  Last Modified: Thu, Oct 11, 2012  6:28:57 PM
+|
+|  Created By: Robert Nelson
+|
+\*-------------------------------------*/
+
+#pragma once
+
+#include "../Instruction.hpp"
+
+class CmpsX : public Instruction {
+	public: 
+		static Instruction* CreateInstruction(unsigned char* memLoc, Processor* proc);
+		int Execute(Processor* proc);
+
+		enum eValidOpcodes {
+			CMPSB = 0xA6,
+			CMPSW = 0xA7,
+		};
+
+	protected:
+		CmpsX(Prefix* pre, std::string text, std::string inst, int op);
+};
