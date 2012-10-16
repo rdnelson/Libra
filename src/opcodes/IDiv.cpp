3e9057ef18008d3f8ab2df4409749e39b1dc4688
Robert Nelson <robert@rnelson.ca>
2012-10-16 19:03:20 -0400

    Added the IDiv opcode

---
+++ src/opcodes/IDiv.cpp
@@ -0,0 +1,91 @@
+/*-------------------------------------*\
+|
+|  File Name: IDiv.cpp
+|
+|  Creation Date: 16-10-2012
+|
+|  Last Modified: Tue, Oct 16, 2012  6:24:07 PM
+|
+|  Created By: Robert Nelson
+|
+\*-------------------------------------*/
+
+#include "IDiv.hpp"
+
+#include "../Processor.hpp"
+#include "../ModrmOperand.hpp"
+
+#include <cstdio>
+
+IDiv::IDiv(Prefix* pre, std::string text, std::string inst, int op) : Instruction(pre,text,inst,op) {}
+
+Instruction* IDiv::CreateInstruction(unsigned char* memLoc, Processor* proc) {
+	unsigned char* opLoc = memLoc;
+	int preLen = 0;
+	char buf[65];
+	std::string inst;
+	Prefix* pre = Prefix::GetPrefix(memLoc);
+
+	if(pre) {
+		opLoc += preLen = pre->GetLength();
+	}
+
+	Instruction* newIDiv = 0;
+
+	if((*opLoc == IDIV_MOD8 || *opLoc == IDIV_MOD16) && 
+			(((*(opLoc + 1) & 0x38) >> 3) == IDIV_SUB_OPCODE)) {
+		unsigned int size = *opLoc == IDIV_MOD8 ? 1 : 2;
+		Operand* dst = ModrmOperand::GetModrmOperand(proc, opLoc, ModrmOperand::MOD, size);
+		snprintf(buf, 65, "IDIV %s", "");
+		GETINST(preLen + 2 + dst->GetBytecodeLen());
+		newIDiv = new IDiv(pre, buf, inst, (int)*opLoc);
+		newIDiv->SetOperand(Operand::DST, dst);
+	}
+	return newIDiv;
+
+}
+
+int IDiv::Execute(Processor* proc) {
+	Operand* dst = mOperands[Operand::DST];
+	if(dst == 0)
+		return -1;
+
+	if(dst->GetValue() == 0) {
+		return IDIV_BY_ZERO;
+	}
+
+	unsigned int dividend = proc->GetRegister(REG_AX) + (mOpcode == IDIV_MOD8 ? 0 :
+	       	proc->GetRegister(REG_DX) << 0x10);
+	unsigned int divisor = dst->GetValue();
+	unsigned int divisorNeg = dst->GetBitmask() == 0xFF ? 0x80 : 0x8000;
+	unsigned int dividendNeg = dst->GetBitmask() == 0xFF ? 0x8000 : 0x80000000;
+	unsigned int dividendBm = dst->GetBitmask() == 0xFF ? 0xFFFF : 0xFFFFFFFF;
+	unsigned int rem, val;
+	bool neg = (((dividend & dividendNeg) >> 8) ^ (divisor & divisorNeg)) != 0;
+	//positivize everything
+	if(dividend & dividendNeg) { //if dividend is negative
+		dividend = (~dividend + 1) & dividendBm; // +ve it and ensure within bitmask
+	}
+	if(divisor & divisorNeg) {
+		divisor = (~divisor + 1) & dst->GetBitmask(); //all operands are positive
+		rem = -(dividend % divisor); //remainder is negative
+	} else {
+		rem = (dividend % divisor); //remainder is positive
+	}
+
+	//calc result
+	val = dividend / divisor; //
+	if(val >= divisorNeg) { //operands are positive, so anything this big is overflow
+		return IDIV_DIV_ERR;
+	}
+	//invert if necessary
+	if(neg)
+		val = -val;
+
+	val &= dst->GetBitmask();
+
+	proc->SetRegister(mOpcode == IDIV_MOD8 ? REG_AL : REG_AX, val);
+	proc->SetRegister(mOpcode == IDIV_MOD8 ? REG_AH : REG_DX, rem);
+
+	return 0;
+}
---
+++ src/opcodes/IDiv.hpp
@@ -0,0 +1,35 @@
+/*-------------------------------------*\
+|
+|  File Name: IDiv.hpp
+|
+|  Creation Date: 16-10-2012
+|
+|  Last Modified: Tue, Oct 16, 2012  2:14:24 PM
+|
+|  Created By: Robert Nelson
+|
+\*-------------------------------------*/
+
+#pragma once
+
+#include "../Instruction.hpp"
+
+class IDiv: public Instruction {
+
+        public:
+                static Instruction* CreateInstruction(unsigned char* memLoc, Processor* proc);
+                int Execute(Processor* proc);
+
+                enum eValidOpcodes {
+                        IDIV_MOD8	= 0xF6,
+                        IDIV_MOD16	= 0xF7,
+                };
+
+		static const unsigned int IDIV_SUB_OPCODE = 0x07;
+		static const unsigned int IDIV_BY_ZERO = 0xDE;
+		static const unsigned int IDIV_DIV_ERR = 0xDE;
+
+        private:
+                IDiv(Prefix* pre, std::string text, std::string inst, int op);
+
+};
