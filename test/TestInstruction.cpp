ad3ad50bcce05a70e3ec14655f85da64aed4461f
Robert Nelson <robert@rnelson.ca>
2012-10-18 17:35:21 -0400

    Updated TestInstruction to new prototype

--- test/TestInstruction.cpp
+++ test/TestInstruction.cpp
@@ -4,7 +4,7 @@
 |
 |  Creation Date: 07-10-2012
 |
-|  Last Modified: Thu, Oct 18, 2012  2:19:29 PM
+|  Last Modified: Thu, Oct 18, 2012  5:34:36 PM
 |
 |  Created By: Robert Nelson
 |
@@ -17,35 +17,35 @@
 
 TEST(InstructionFlagsTest, OverflowAdd16) {
 
-	ASSERT_FALSE(Instruction::OverflowAdd(0, 0, 0, 2));
-	ASSERT_FALSE(Instruction::OverflowAdd(0x7FFF, 0x7FFF, 0, 2)); // 0x7FFF (32767)
-	ASSERT_TRUE(Instruction::OverflowAdd(0x8000, 0x7FFF, 1, 2)); // 0x8000 (-32768)
-	ASSERT_TRUE(Instruction::OverflowAdd(0x8000, 0x4000, 0x4000, 2)); // 0x8000 (-32768)
-	ASSERT_FALSE(Instruction::OverflowAdd(0xFFFD, 0xFFFF, 0xFFFE, 2)); // 0xFFFD (-3)
-	ASSERT_TRUE(Instruction::OverflowAdd(0x7FFF, 0xFFFF, 0x8000, 2)); // 0x7FFF (32767)
-	ASSERT_FALSE(Instruction::OverflowAdd(0x0000, 0xFFFF, 0x0001, 2)); // 0x0000 (0)
+	ASSERT_FALSE(Instruction::OverflowAdd(0, 0, 2));
+	ASSERT_FALSE(Instruction::OverflowAdd(0x7FFF, 0, 2)); // 0x7FFF (32767)
+	ASSERT_TRUE(Instruction::OverflowAdd(0x7FFF, 1, 2)); // 0x8000 (-32768)
+	ASSERT_TRUE(Instruction::OverflowAdd(0x4000, 0x4000, 2)); // 0x8000 (-32768)
+	ASSERT_FALSE(Instruction::OverflowAdd(0xFFFF, 0xFFFE, 2)); // 0xFFFD (-3)
+	ASSERT_TRUE(Instruction::OverflowAdd(0xFFFF, 0x8000, 2)); // 0x7FFF (32767)
+	ASSERT_FALSE(Instruction::OverflowAdd(0xFFFF, 0x0001, 2)); // 0x0000 (0)
 }
 
 TEST(InstructionFlagsTest, OverflowAdd8) {
 
-	ASSERT_FALSE(Instruction::OverflowAdd(0, 0, 0, 1));
-	ASSERT_FALSE(Instruction::OverflowAdd(0x7F, 0x7F, 0, 1)); // 0x7F (127)
-	ASSERT_TRUE(Instruction::OverflowAdd(0x80, 0x7F, 1, 1)); // 0x80 (-128)
-	ASSERT_TRUE(Instruction::OverflowAdd(0x80, 0x40, 0x40, 1)); // 0x80 (-128)
-	ASSERT_FALSE(Instruction::OverflowAdd(0xFD, 0xFF, 0xFE, 1)); // 0xFD (-3)
-	ASSERT_TRUE(Instruction::OverflowAdd(0x7F, 0xFF, 0x80, 1)); // 0x7F (127)
-	ASSERT_FALSE(Instruction::OverflowAdd(0x00, 0xFF, 0x01, 1)); // 0x00 (0)
+	ASSERT_FALSE(Instruction::OverflowAdd(0, 0, 1));
+	ASSERT_FALSE(Instruction::OverflowAdd(0x7F, 0, 1)); // 0x7F (127)
+	ASSERT_TRUE(Instruction::OverflowAdd(0x7F, 1, 1)); // 0x80 (-128)
+	ASSERT_TRUE(Instruction::OverflowAdd(0x40, 0x40, 1)); // 0x80 (-128)
+	ASSERT_FALSE(Instruction::OverflowAdd(0xFF, 0xFE, 1)); // 0xFD (-3)
+	ASSERT_TRUE(Instruction::OverflowAdd(0xFF, 0x80, 1)); // 0x7F (127)
+	ASSERT_FALSE(Instruction::OverflowAdd(0xFF, 0x01, 1)); // 0x00 (0)
 
 }
 
 TEST(InstructionFlagsTest, OverflowSub8) {
 
-	ASSERT_FALSE(Instruction::OverflowSub(0, 0, 0, 1));
-	ASSERT_FALSE(Instruction::OverflowSub(0x7F, 0x7F, 0x00, 1)); // 0x7F (127)
-	ASSERT_FALSE(Instruction::OverflowSub(0x7E, 0x7F, 0x01, 1)); // 0x7E (126)
-	ASSERT_FALSE(Instruction::OverflowSub(0xFF, 0x00, 0x01, 1)); // 0xFF (-3)
-	ASSERT_TRUE(Instruction::OverflowSub(0x80, 0x7F, 0xFF, 1)); // 0x7F (32767)
-	ASSERT_TRUE(Instruction::OverflowSub(0x7F, 0x80, 0x01, 1)); // 0x7F (32767)
+	ASSERT_FALSE(Instruction::OverflowSub(0, 0, 1));
+	ASSERT_FALSE(Instruction::OverflowSub(0x7F, 0x00, 1)); // 0x7F (127)
+	ASSERT_FALSE(Instruction::OverflowSub(0x7F, 0x01, 1)); // 0x7E (126)
+	ASSERT_FALSE(Instruction::OverflowSub(0x00, 0x01, 1)); // 0xFF (-3)
+	ASSERT_TRUE(Instruction::OverflowSub(0x7F, 0xFF, 1)); // 0x7F (32767)
+	ASSERT_TRUE(Instruction::OverflowSub(0x80, 0x01, 1)); // 0x7F (32767)
 
 }
 
