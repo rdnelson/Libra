e5b6160f9f83a3a250d3d222d7bb7f2804b67fd8
Robert Nelson <robert@rnelson.ca>
2012-10-20 18:06:25 -0400

    Fixed signed/unsigned comparison

--- src/opcodes/Push.cpp
+++ src/opcodes/Push.cpp
@@ -37,7 +37,7 @@ Instruction* Push::CreateInstruction(unsigned char* memLoc, Processor* proc) {
 	switch(*opLoc) {
 		case PUSH_MOD16:
 		{
-			if((*(opLoc + 1) & 0x38) >> 3 != PUSH_SUB_OPCODE)
+			if(((*(opLoc + 1) & 0x38) >> 3) != PUSH_SUB_OPCODE)
 				return newPush;
 			Operand* dst = ModrmOperand::GetModrmOperand(proc, opLoc, ModrmOperand::MOD, 2);
 			snprintf(buf, 65, "PUSH %s", dst->GetDisasm().c_str());
