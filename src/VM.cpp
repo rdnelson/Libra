85b55da7ab065a53104c5c8c95ba53b026b036f4
Robert Nelson <robert@rnelson.ca>
2012-10-23 09:41:23 -0400

    Temp commit

--- src/VM.cpp
+++ src/VM.cpp
@@ -23,8 +23,9 @@ VM::VM(int argc, char* argv[]) : mProc(mMem) {
 
 	//initialize main memory
 	memset(mMem, 0xFF, MEM_SIZE);
+	Instruction::InitializeOpcodes();
 
-	LoadObjectFile(argv[1]);
+	LoadVirgoFile(argv[1]);
 }
 
 
@@ -32,7 +33,11 @@ VM::VM(int argc, char* argv[]) : mProc(mMem) {
 // The object file will be copied into memory at address 0x0000
 // Execution will begin at address 0x0000 by default
 
-int VM::LoadObjectFile(char* filename) {
+int VM::LoadFlatFile(const char* filename) {
+
+	//reset everything
+	mLoaded = false;
+	mProc.Initialize();
 
 	std::ifstream fin;
 	fin.open(filename, std::ios_base::in | std::ios_base::binary);
@@ -42,6 +47,9 @@ int VM::LoadObjectFile(char* filename) {
 
 	int i = 0;
 	while(!fin.eof()) {
+		if(((i + 1) * 1024) >= MEM_SIZE)
+			return VM_ERR_BIG_FILE;
+
 		fin.read((char*)(mMem + (i++) * 1024), 1024);
 		if(fin.bad()) { //an error occurred
 			fin.close();
@@ -49,10 +57,99 @@ int VM::LoadObjectFile(char* filename) {
 		}
 	};
 
+	mLoaded = true;
+	mVirgo = false;
+
 	return VM_SUCCESS;
 
 }
 
+int VM::LoadVirgoFile(const char* filename) {
+
+	mLoaded - false;
+	mProc.Initialize();
+
+	std::ifstream fin;
+	fin.open(filename, std::ios_base::in);
+	if(fin.fail()) {
+		return VM_ERR_FOPEN;
+	}
+
+	//parse header
+	int numInitArgs, numLines, addrStart, bytesTotal;
+	char delim;
+	fin >> numInitArgs;
+	fin >> numLines;
+	fin >> addrStart;
+	fin >> bytesTotal;
+
+	int i = 0;
+	int hexSize;
+	char hex[32];
+	char label[128];
+	char disasm[256];
+	int addr;
+	int delay;
+	char line[512];
+	memset(line, 0, 512);
+	fin.ignore(100, '\n');
+
+	while(!fin.eof()) {
+		fin.getline(line, 512);
+		for(char* c = line; *c != '\0'; c++)
+			*c = *c == '\x001f' ? '\0' : *c;
+		hex[0] = '\0';
+		label[0] = '\0';
+		disasm[0] = '\0';
+		delay = 0;
+		char* cl = line;
+		cl++;
+		if(strlen(cl) != 0)
+			sscanf(cl, "%d", &addr);
+		cl += strlen(cl) + 2;
+
+		if(strlen(cl) != 0)
+			sscanf(cl, "%d", &hexSize);
+		cl += strlen(cl) + 2;
+
+		if(strlen(cl) != 0)
+			strncpy(hex, cl, 32);
+		cl += strlen(cl) + 2;
+
+		if(strlen(cl) != 0)
+			strncpy(label, cl, 128);
+		cl += strlen(cl) + 2;
+
+		if(strlen(cl) != 0)
+			strncpy(disasm, cl, 256);
+		cl += strlen(cl) + 2;
+
+		if(strlen(cl) != 0)
+			sscanf(cl, "%d", &delay);
+		
+
+		//sscanf(line, "%d\x001f\x001f%d\x001f\x001f%[^\x001f]\x001f\x001f%[^\x001f]\x001f\x001f%[^\x001f]\x001f\x001f%d\x001f", &addr, &hexSize, hex, label, disasm, &delay);
+		//sscanf(line, "%d%*[\n]%d%*[\n]%s%*[\n]%s%*[\n]%[^\n]%*[\n]%d", &addr, &hexSize, hex, label, disasm, &delay);
+
+
+		if(++i >= numLines)
+			break;
+
+	}
+}
+
+void VM::Disassemble() {
+    if(mLoaded) {
+        if(!mVirgo) {
+            unsigned int tmpIP = 0;
+            Instruction* tmpInst = 0;
+            while(tmpInst = Instruction::ReadInstruction(mMem + tmpIP, &mProc )) {
+                mInstructions.push_back(tmpInst);
+                tmpIP += tmpInst->GetLength() % MEM_SIZE;
+            }
+        }
+    }
+}
 
 
 int VM::Run() {
