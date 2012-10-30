e4a0bcfd63dc401819471a252c25612cd24a6524
Robert Nelson <robert@rnelson.ca>
2012-10-30 10:44:10 -0400

    Fixed cli version not running virgo files

--- src/VM.cpp
+++ src/VM.cpp
@@ -66,7 +66,7 @@ int VM::LoadFlatFile(const char* filename) {
 
 int VM::LoadVirgoFile(const char* filename) {
 
-	mLoaded - false;
+	mLoaded = false;
 	mProc.Initialize();
 
 	std::ifstream fin;
@@ -84,35 +84,44 @@ int VM::LoadVirgoFile(const char* filename) {
 
 	int i = 0;
 	int hexSize;
-	char hex[32];
+	char* hex;
 	char label[128];
 	char disasm[256];
 	int addr;
 	int delay;
 	char line[512];
+	char text[20];
 	memset(line, 0, 512);
 	fin.ignore(100, '\n');
 
 	while(!fin.eof()) {
 		fin.getline(line, 512);
+		if(fin.eof())
+			break;
 		for(char* c = line; *c != '\0'; c++)
 			*c = *c == '\x001f' ? '\0' : *c;
-		hex[0] = '\0';
+		hex = 0;
 		label[0] = '\0';
 		disasm[0] = '\0';
 		delay = 0;
 		char* cl = line;
 		cl++;
 		if(strlen(cl) != 0)
-			sscanf(cl, "%d", &addr);
+			sscanf(cl, "%x", &addr);
 		cl += strlen(cl) + 2;
 
 		if(strlen(cl) != 0)
 			sscanf(cl, "%d", &hexSize);
 		cl += strlen(cl) + 2;
 
-		if(strlen(cl) != 0)
-			strncpy(hex, cl, 32);
+		unsigned int hexArrSize = (hexSize > 1024 ? 1024 : (hexSize == 0 ? 1 : (hexSize * 2 + 1)) * sizeof(char));
+		hex = new char[hexArrSize];
+
+		if(strlen(cl) != 0) {
+			strncpy(hex, cl, hexArrSize);
+		} else {
+			hex[0] = '\0';
+		}
 		cl += strlen(cl) + 2;
 
 		if(strlen(cl) != 0)
@@ -125,16 +134,52 @@ int VM::LoadVirgoFile(const char* filename) {
 
 		if(strlen(cl) != 0)
 			sscanf(cl, "%d", &delay);
-		
 
-		//sscanf(line, "%d\x001f\x001f%d\x001f\x001f%[^\x001f]\x001f\x001f%[^\x001f]\x001f\x001f%[^\x001f]\x001f\x001f%d\x001f", &addr, &hexSize, hex, label, disasm, &delay);
-		//sscanf(line, "%d%*[\n]%d%*[\n]%s%*[\n]%s%*[\n]%[^\n]%*[\n]%d", &addr, &hexSize, hex, label, disasm, &delay);
+
+		if(strlen(hex) % 2 != 0 || strlen(hex) / 2 != hexSize) { //odd number of hex digits, not bytes
+			delete hex;
+			mLoaded = false;
+			return VM_ERR_CORRUPT;
+		}
+
+		memset(text, 0, 20);
+		for(int j = 0; j < strlen(hex); j++) {
+			if(hex[j] >= '0' && hex[j] <= '9') {
+				text[j/2] |= (hex[j] - '0') << (j % 2 == 0 ? 4 : 0);
+			}else if(toupper(hex[j])  >= 'A' && toupper(hex[j]) <= 'F') {
+				text[j/2] |= (hex[j] - 'A' + 10) << (j % 2 == 0 ? 4 : 0);
+			}
+		}
+
+		delete hex;
+
+		Prefix* pre = Prefix::GetPrefix((unsigned char*)text);
+		char* opLoc = text;
+		if(pre) {
+			opLoc += pre->GetLength();
+		}
+
+		std::string s;
+		std::string dis(disasm);
+
+		dis.erase(dis.find_last_not_of(" \f\n\r\t\v") + 1);
+		dis.erase(0, dis.find_first_not_of(" \f\n\r\t\v"));
+
+		s.insert(0, text, hexSize);
+		if(dis.size() == 0)
+			continue;
+
+
+		memcpy(mMem + (addr % MEM_SIZE), text, hexSize);
 
 
 		if(++i >= numLines)
 			break;
 
 	}
+
+	mLoaded = true;
+	mVirgo = true;
 
 	return VM_SUCCESS;
 }
