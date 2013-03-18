7561f3007d19fa89112508dcb97fdf69a2091626
Robert Nelson <robert@rnelson.ca>
2013-03-18 12:22:36 -0400

    Prevent register overflow

--- src/Register.hpp
+++ src/Register.hpp
@@ -17,11 +17,11 @@
 
 class Register {
 
-	public: 
+	public:
         Register() : mVal(0) { strncpy(mHex, "0000", 7); }
         unsigned int GetValue() const { return mVal; }
-        void SetValue(unsigned int newVal) { mVal = newVal; snprintf(mHex, 7, "%04X", mVal); }
-        void AddValue(unsigned int addVal) { mVal += addVal; snprintf(mHex, 7, "%04X", mVal); }
+        void SetValue(unsigned int newVal) { mVal = newVal & 0xFFFF; snprintf(mHex, 7, "%04X", mVal); }
+        void AddValue(unsigned int addVal) { mVal += addVal; mVal &= 0xFFFF; snprintf(mHex, 7, "%04X", mVal); }
         const char* GetHex() const {
             return mHex;
         }
