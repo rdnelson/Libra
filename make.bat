08377c4f82e9c3d453c9a82923ab605bc2c23c87
Robert Nelson <robert@rnelson.ca>
2013-04-08 20:44:03 -0400

    Fixed incorrect build parameter

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 call %SDK_DIR%\bin\SetEnv.cmd /Debug
 cd msvc10
-msbuild Libra.sln /nologo /verbosity:m /p:Debug
+msbuild Libra.sln /nologo /verbosity:m /p:Configuration=Debug
 call %SDK_DIR%\bin\SetEnv.cmd /Release
-msbuild Libra.sln /nologo /verbosity:m /p:Release
+msbuild Libra.sln /nologo /verbosity:m /p:Configuration=Release
 cd ..
 bin\LibraTest.exe --gtest_output=xml
