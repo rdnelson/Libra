518a6e21f816caeb80525901c588a0519abf6d41
Robert Nelson <robert@rnelson.ca>
2013-07-02 02:04:28 -0700

    Environment now set properly for windows builds

--- make.bat
+++ make.bat
@@ -1,10 +1,9 @@
 mkdir bin
 del /Q bin\*
-call %SDK_DIR%\bin\SetEnv.cmd /Debug
-setenv /x86
+call %SDK_DIR%\bin\SetEnv.cmd /x86 /Debug
 cd msvc10
 msbuild Libra.sln /nologo /verbosity:m /p:Configuration=Debug
-call %SDK_DIR%\bin\SetEnv.cmd /Release
+call %SDK_DIR%\bin\SetEnv.cmd /x86 /Release
 msbuild Libra.sln /nologo /verbosity:m /p:Configuration=Release
 cd ..
 bin\LibraTest.exe --gtest_output=xml
