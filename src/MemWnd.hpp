2e52bacb9db4601e86b120912069499da12988ea
Robert Nelson <robert@rnelson.ca>
2013-10-07 12:09:11 -0400

    Migrated build system over to CMake. Added version dialog to Libra.

--- src/MemWnd.hpp
+++ src/MemWnd.hpp
@@ -39,6 +39,7 @@ public slots:
 	void enableMemoryLogging_Clicked();
 	void lstInstructions_RightClicked(const QPoint&);
     void openHelp();
+    void openAbout();
     void deviceError(IPeripheral*, unsigned int);
 
 	//VMWorker Slots
